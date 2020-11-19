670a85a0a7ca3c96cd8a3f855ac655a27c36465a
saifedine hajji <saifedine.hajji@gmail.com>
2020-11-19 16:29:33 +0100

    Created angular project and completed feature 1: Spring Rest GET books

--- onlinebookstore/src/main/java/com/myProjects/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/myProjects/onlinebookstore/repository/BookRepository.java
@@ -1,11 +1,11 @@
 package com.myProjects.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.myProjects.onlinebookstore.entity.Book;
 
-
+@CrossOrigin("*")
 public interface BookRepository extends JpaRepository<Book,Long> {
 
 }
